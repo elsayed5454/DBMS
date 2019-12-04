505e2c14e421e8572d92d6af33bdd37c3d405b39
Elsayed Akram <elsayed5454@gmail.com>
2019-12-04 16:12:27 +0200

    Pull console class

--- src/eg/edu/alexu/csd/oop/db/cs33/Console.java
+++ src/eg/edu/alexu/csd/oop/db/cs33/Console.java
@@ -5,8 +5,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
